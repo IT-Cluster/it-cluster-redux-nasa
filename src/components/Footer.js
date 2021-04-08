9193c07b5c8686dda4aa8a9b0296a04b849f1f58
Zoreslav <gzoreslav@gmail.com>
2021-04-08 19:56:16 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default () => (
     <footer className="text-muted">
         <div className="container">
-            <p>React-Redux NASA API Demo project</p>
+            <p>React-Redux NASA API Demo project | Unit Tests | Thunk | Saga | CI/CD</p>
         </div>
     </footer>
 );
